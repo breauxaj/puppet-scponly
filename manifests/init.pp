1df853dbf3cb0617180a9c50e94489716be1b5b4
Adam Breaux <breauxaj@gmail.com>
2013-10-16 09:51:36 -0400

    wrong var?

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class scponly {
   package { 'marmotte-scponly':
     ensure   => installed,
     provider => rpm,
-    name     => 'http://dl.marmotte.net/rpms/redhat/el6/x86_64/scponly-4.8-1.el6/scponly-4.8-1.el6.x86_64.rpm',
+    source   => 'http://dl.marmotte.net/rpms/redhat/el6/x86_64/scponly-4.8-1.el6/scponly-4.8-1.el6.x86_64.rpm',
   }
 
 }
