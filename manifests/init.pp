71631ac281b1012a0713fc22e5e2145872b852ba
Adam Breaux <breauxaj@gmail.com>
2013-10-16 09:55:08 -0400

    package name issue?

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class scponly {
-  package { 'marmotte-scponly':
+  package { 'scponly':
     ensure   => installed,
     provider => rpm,
     source   => 'http://dl.marmotte.net/rpms/redhat/el6/x86_64/scponly-4.8-1.el6/scponly-4.8-1.el6.x86_64.rpm',
